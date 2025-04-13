57e71cc0889b5971cdda0c87aa299724e59ff0b6
Alicia <alicia.rsouza@gmail.com>
2025-04-13 10:54:53 -0300

    adicao de buscar produtos com autenticacao

--- src/main/java/br/com/dummy/cliente/ClienteProdutos.java
+++ src/main/java/br/com/dummy/cliente/ClienteProdutos.java
@@ -1,18 +1,36 @@
 package br.com.dummy.cliente;
 
-import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 
 import static io.restassured.RestAssured.given;
 
 public class ClienteProdutos {
     private static final String CAMINHO_PRODUTOS = "/products";
+    private static final String CAMINHO_PRODUTOS_AUTENTICACAO = "auth/products";
 
-    public static Response consultaProdutos (RequestSpecification configuracaoRequisicao){
+    public static ValidatableResponse consultaProdutos (RequestSpecification configuracaoRequisicao,
+                                                        ResponseSpecification configuracaoResposta){
         return given().
                 spec(configuracaoRequisicao).
                when().
-                get(CAMINHO_PRODUTOS);
+                get(CAMINHO_PRODUTOS).
+               then().
+                spec(configuracaoResposta);
     }
+
+    public static ValidatableResponse consultaProdutosComAutenticacao (RequestSpecification configuracaoRequisicao,
+                                                                       ResponseSpecification configuracaoResposta,
+                                                                       String token){
+        return given().
+                 spec(configuracaoRequisicao).
+                 header("Authorization", "Bearer " + token).
+                when().
+                 get(CAMINHO_PRODUTOS_AUTENTICACAO).
+                then().
+                 spec(configuracaoResposta);
+    }
+
 
 }
